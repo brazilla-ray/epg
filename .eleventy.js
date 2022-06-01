e2dd5140f7dfdd2eb0b38f1565fb10a58023e8af
William Whitaker <billydwhitaker@gmail.com>
2022-06-01 15:27:59 -0400

    adds and sets up sass (fixes npm scripts etc.)

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./src/scss');
   // Copy `src/style.css` to `_site/style.css`
-  eleventyConfig.addPassthroughCopy("src/style.css");
-  eleventyConfig.addPassthroughCopy("src/index.js");
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/index.js');
   return {
     // When a passthrough file is modified, rebuild the pages:
     passthroughFileCopy: true,
     dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
   };
 };
