162cdd79ca7e47c924867b58156d98b293890483
William Whitaker <billydwhitaker@gmail.com>
2022-06-01 17:16:29 -0400

    corrects passthrough etc.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./src/scss');
-  // Copy `src/style.css` to `_site/style.css`
+  eleventyConfig.addWatchTarget('./src/scss/');
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/index.js');
   return {
