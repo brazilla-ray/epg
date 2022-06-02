2cf70cafeb5d199c124d83c7a975460252cb4e8f
William Whitaker <billydwhitaker@gmail.com>
2022-06-01 20:02:08 -0400

    cleans up watch tasks

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/scss/');
-  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/css/style.css');
   eleventyConfig.addPassthroughCopy('src/index.js');
+  eleventyConfig.setUseGitIgnore(false);
   return {
     // When a passthrough file is modified, rebuild the pages:
     passthroughFileCopy: true,
